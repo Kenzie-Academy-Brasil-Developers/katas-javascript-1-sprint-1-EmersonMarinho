6533fd41bc03f497cafc22f8417b63a93538f7a1
Emerson <emerson_marinhoaz@hotmail.com>
2021-08-08 21:03:05 -0300

    finalizado

--- katas1.js
+++ katas1.js
@@ -2,105 +2,152 @@ function oneThroughTwenty() {
   
   let onetwenty = [];
   
-  for (let i = 1; i <= 21; i++){
-    console.log(i)}
+  for (let i = 1; i <= 20; i++){
+    console.log(i)
+  
+  }
+  return onetwenty;
 }
 
-  return onetwenty;
+console.log(oneThroughTwenty()); 
 
 //call function oneThroughTwenty
 
 function evensToTwenty() {
-    
-  for (let i = 1; i <= 21; i++){
-    if (i % 2 !==1){
-    console.log(i)}}
   
+  let pair20 = [];
+  
+  for (let i = 1; i <= 20; i++){
+    if (i % 2 !==1)
+    console.log(i)
+  
+  }
+  return pair20;
 }
+
+console.log(evensToTwenty()); 
 
 //call function evensToTwenty
 
 function oddsToTwenty() {
   
-  for (let i = 1; i <= 21; i++){
-    if (i % 2 !==0){
-    console.log(i)}}
+  let unpair20 = [];
+  
+  for (let i = 1; i <= 20; i++){
+    if (i % 2 !==0)
+    console.log(i)
+  
+  }
+  return unpair20;
 }
+
+console.log(oddsToTwenty()); 
 
 //call function oddsToTwenty
 
 function multiplesOfFive() {
-    
-  for (let i = 5; i <= 105; i+=5 ){
-    console.log(i)}
-
+  
+  let fiveplus = [];
+  
+  for (let i = 5; i <= 100; i+=5){
+    console.log(i)
+  
+  }
+  return fiveplus;
 }
+
+console.log(multiplesOfFive()); 
 
 //call function multiplesOfFive
 
 function squareNumbers() {
-    
-  for (let i = 1; i <= 11; i++){
-    console.log(i * i)}
-    
+  
+  let squared = [];
+  
+  for (let i = 0; i <= 10; i++){
+    console.log(i * i)
+  
+  }
+  return squared;
 }
+
+console.log(squareNumbers()); 
 
 //call function squareNumbers
 
 function countingBackwards() {
-    
+  
+  let backnumber = [];
+  
   for (let i = 20; i >= 1; i--){
-    console.log(i)}
+    console.log(i)
+  
+  }
+  return backnumber;
 }
+
+console.log(countingBackwards()); 
 
 //call function countingBackwards
 
 function evenNumbersBackwards() {
-    
+  
+  let pairback = [];
+  
   for (let i = 20; i >= 1; i--){
-    if (i % 2 !== 1){
-    console.log(i)}}
+    if ( i % 2 !==1)
+    console.log(i)
+  
+  }
+  return pairback;
 }
+
+console.log(evenNumbersBackwards()); 
 
 //call function evenNumbersBackwards
 
 function oddNumbersBackwards() {
-    
+  
+  let unpairback = [];
+  
   for (let i = 20; i >= 1; i--){
-    if (i % 2 !== 0){
-    console.log(i)}}
-
+    if ( i % 2 !==0)
+    console.log(i)
+  
+  }
+  return unpairback;
 }
+
+console.log(oddNumbersBackwards()); 
 
 //call function oddNumbersBackwards
 
 function multiplesOfFiveBackwards() {
   
-  for (let i = 100; i >= 0; i-=5 ){
+  let fiveback = [];
+  
+  for (let i = 100; i >= 5; i-=5){
     console.log(i)
+  
+  }
+  return fiveback;
 }
-}
+
+console.log(multiplesOfFiveBackwards());
 
 //call function multiplesOfFiveBackwards
 
 function squareNumbersBackwards() {
-    
+  
+  let squareback = [];
+  
   for (let i = 10; i >= 2; i--){
-  console.log(i * i)
-}   
+    console.log(i)
+  
+  }
+  return squareback;
 }
+
+console.log(squareNumbersBackwards());
 
 //call function squareNumbersBackwards
-
-function oneThroughFive() {
-
-  let meuRetorno = [];
-
-  for (let counter = 1; counter <= 5; counter++) {
-      //código
-  }
-
-  return meuRetorno;
-}
-
-console.log(oneThroughFive());
